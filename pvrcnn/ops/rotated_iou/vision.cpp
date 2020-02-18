d9e5bee7521607c915ac4c86f1e32ee3a6a3ad14
jhultman <27909223+jhultman@users.noreply.github.com>
2020-02-17 21:12:54 -0500

    add rotated_iou from detectron2

---
+++ pvrcnn/ops/rotated_iou/vision.cpp
@@ -0,0 +1,65 @@
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+#include <torch/extension.h>
+#include "box_iou_rotated.h"
+
+namespace detectron2 {
+
+#ifdef WITH_CUDA
+extern int get_cudart_version();
+#endif
+
+std::string get_cuda_version() {
+#ifdef WITH_CUDA
+  std::ostringstream oss;
+
+  // copied from
+  // https://github.com/pytorch/pytorch/blob/master/aten/src/ATen/cuda/detail/CUDAHooks.cpp#L231
+  auto printCudaStyleVersion = [&](int v) {
+    oss << (v / 1000) << "." << (v / 10 % 100);
+    if (v % 10 != 0) {
+      oss << "." << (v % 10);
+    }
+  };
+  printCudaStyleVersion(get_cudart_version());
+  return oss.str();
+#else
+  return std::string("not available");
+#endif
+}
+
+// similar to
+// https://github.com/pytorch/pytorch/blob/master/aten/src/ATen/Version.cpp
+std::string get_compiler_version() {
+  std::ostringstream ss;
+#if defined(__GNUC__)
+#ifndef __clang__
+
+#if ((__GNUC__ <= 4) && (__GNUC_MINOR__ <= 8))
+#error "GCC >= 4.9 is required!"
+#endif
+
+  { ss << "GCC " << __GNUC__ << "." << __GNUC_MINOR__; }
+#endif
+#endif
+
+#if defined(__clang_major__)
+  {
+    ss << "clang " << __clang_major__ << "." << __clang_minor__ << "."
+       << __clang_patchlevel__;
+  }
+#endif
+
+#if defined(_MSC_VER)
+  { ss << "MSVC " << _MSC_FULL_VER; }
+#endif
+  return ss.str();
+}
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  m.def("get_compiler_version", &get_compiler_version, "get_compiler_version");
+  m.def("get_cuda_version", &get_cuda_version, "get_cuda_version");
+  m.def("rotated_iou", &box_iou_rotated, "IoU for rotated boxes");
+}
+
+} // namespace detectron2
--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
-from setuptools import setup
+import glob
+from setuptools import setup, find_packages
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-setup(
-    name='pvrcnn',
-    version='0.1',
-    description='Implementation of PV-RCNN algorithm',
-    author='Jacob Hultman',
-    packages=['pvrcnn'],
-    install_requires=['numpy', 'torch', 'yacs', 'tqdm', 'spconv', 'pointnet2', 'torchsearchsorted'],
-)
+if __name__ == '__main__':
+    setup(
+        name='pvrcnn',
+        version='0.1',
+        description='Implementation of PV-RCNN algorithm',
+        author='Jacob Hultman',
+        packages=find_packages(),
+        package_data={'pvrcnn.ops': ['*/*.so']},
+        install_requires=[
+            'numpy', 'torch', 'yacs', 'tqdm', 'spconv', 'pointnet2', 'torchsearchsorted'],
+        ext_modules=[
+            CUDAExtension(
+                'pvrcnn._C',
+                glob.glob('pvrcnn/ops/rotated_iou/*.cpp') +
+                glob.glob('pvrcnn/ops/rotated_iou/*.cu'),
+                extra_compile_args={'cxx': ['-g',], 'nvcc': ['-g',],},
+            ),
+        ],
+        cmdclass={'build_ext': BuildExtension},
+    )
